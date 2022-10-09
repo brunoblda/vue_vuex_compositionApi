d6ba2be70c66b25448071cc0c5f3bbcf11d5fe1e
Bruno Luiz de Deus Adão <brunoblda@gmail.com>
2022-10-09 18:36:30 -0300

    vue vuex composition api

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,84 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <p>{{count}}</p>
+  <button @click="click">Increment</button>
+  <p></p>
+  <button 
+    v-for="post in posts"
+    :key="post.id"
+    @click="click2(post)"
+  >
+    {{post.title}}
+  </button>
+<p></p>
+  <button 
+    v-for="post in posts2"
+    :key="post.id"
+    @click="click3(post)"
+  >
+    {{post.title}}
+  </button>
+
+  {{postId}}
+
+  <div
+    v-if="currentPost"
+  >
+    <h2>{{currentPost.title}}</h2>
+    <h4>{{currentPost.content}}</h4>
+
+  </div>
+
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { computed, defineComponent, onMounted } from 'vue';
+import { useStore } from 'vuex';
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
+  setup(){
+
+    const posts = [
+      { id: 1, title: 'Post #1'},
+      { id: 2, title: 'Post #2'},
+    ]
+
+    const store = useStore() 
+
+    const click = () => {
+      store.commit('posts/increment', 10)
+    }
+
+    const click2 = (post: any) => {
+      store.commit('posts/setPostId', post.id)
+    }
+
+    const click3 = (post: any) => {
+      store.commit('posts/setPostId', post.id)
+    }
+
+    console.log(store)
+    console.log(store.state.posts.count)
+
+    const fetchData = () => {
+      store.dispatch('posts/fetch', 'Post')
+    }
+
+    onMounted(()=> {
+      fetchData()
+    }) 
+
+    return {
+      postId: computed(() => store.state.posts.postId),
+      count: computed(() => store.state.posts.count),
+      click,
+      posts,
+      click2,
+      fetchData,
+      click3,
+      posts2 : computed(() => store.state.posts.all),
+      currentPost: computed(() => store.getters['posts/currentPost'])
+    }
   }
 });
 </script>
